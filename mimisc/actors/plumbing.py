acb9f0bf1ccb20696747cce39eda16ae2379bc33
Jannis Harder <jix@jixco.de>
2014-11-18 00:13:47 +0100

    add actors.plumbing with split, gather and relax

--- mimisc/actors/plumbing.py
+++ mimisc/actors/plumbing.py
@@ -52,3 +52,76 @@ class Relax(Module):
                 )
             )
         ]
+
+
+class Schedule(Module):
+    def __init__(self, layout):
+        self.sink = Sink(layout)
+        self.source_a = Source(layout)
+        self.source_b = Source(layout)
+        self.busy = Signal()
+
+        ###
+
+        priority = Signal()
+
+        en = Signal()
+        en_a = Signal()
+        en_b = Signal()
+
+        self.comb += [
+            self.busy.eq(0),
+            en.eq((self.source_a.ack | self.source_b.ack) & self.sink.stb),
+            self.sink.ack.eq(en),
+            If(priority,
+                en_a.eq(en & self.source_a.ack),
+                en_b.eq(en & ~self.source_a.ack),
+            ).Else(
+                en_a.eq(en & ~self.source_b.ack),
+                en_b.eq(en & self.source_b.ack),
+            ),
+            self.source_a.stb.eq(en_a),
+            self.source_b.stb.eq(en_b),
+            self.source_a.payload.eq(self.sink.payload),
+            self.source_b.payload.eq(self.sink.payload),
+        ]
+
+        self.sync += If(en, priority.eq(~priority))
+
+
+class Gather(Module):
+    def __init__(self, layout):
+        self.sink_a = Sink(layout)
+        self.sink_b = Sink(layout)
+        self.source = Source(layout)
+        self.busy = Signal()
+
+        ###
+
+        priority = Signal()
+
+        en = Signal()
+        en_a = Signal()
+        en_b = Signal()
+
+        self.comb += [
+            self.busy.eq(0),
+            en.eq((self.sink_a.stb | self.sink_b.stb) & self.source.ack),
+            self.source.stb.eq(en),
+            If(priority,
+                en_a.eq(en & self.sink_a.stb),
+                en_b.eq(en & ~self.sink_a.stb),
+            ).Else(
+                en_a.eq(en & ~self.sink_b.stb),
+                en_b.eq(en & self.sink_b.stb),
+            ),
+            self.sink_a.ack.eq(en_a),
+            self.sink_b.ack.eq(en_b),
+            If(en_a,
+                self.source.payload.eq(self.sink_a.payload),
+            ).Else(
+                self.source.payload.eq(self.sink_b.payload),
+            ),
+        ]
+
+        self.sync += If(en, priority.eq(~priority))
